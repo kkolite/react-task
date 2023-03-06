c412a23a4f9af350827118d64d27593cb0c65dbb
Yahor Litavar <sergioegor21@gmail.com>
2023-03-06 12:56:12 +0300

    feat: add fetch

--- vite-project/src/data/types.ts
+++ vite-project/src/data/types.ts
@@ -4,3 +4,21 @@ export interface IInputProps {
   value?: string,
   onChange?: () => void
 }
+
+export enum EFetchParams {
+  NAME = 'name',
+  ICAO = 'icao',
+  IATA = 'iata'
+}
+
+interface IFleet {
+  [index: string]: number
+}
+
+export interface IAirline {
+  iata: string,
+  icao: string,
+  logo_url: string,
+  name: string,
+  fleet: IFleet
+}
