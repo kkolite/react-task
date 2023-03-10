00a1a4517363c2779c2534671bbfbebb872d9624
Yahor Litavar <sergioegor21@gmail.com>
2023-03-10 15:03:49 +0300

    feat: update about page

--- vite-project/src/pages/About.tsx
+++ vite-project/src/pages/About.tsx
@@ -1,7 +1,14 @@
 const About = () => {
   return (
-    <div>
-      Abot this app
+    <div className="about__page">
+      <h1>About</h1>
+      <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut quis eius facere ullam deleniti? Voluptatum tempora dolorum iure dolor unde? Consequuntur odio possimus suscipit eos mollitia inventore doloribus recusandae corrupti?
+      </div>
+      <h2>Our mission</h2>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, repudiandae amet aut aliquam repellat molestiae id explicabo quidem voluptatum sequi tenetur consequatur sapiente ipsa ipsam architecto, et blanditiis laudantium assumenda.
+      </div>
     </div>
   );
 };
