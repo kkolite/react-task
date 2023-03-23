747e70bdba216957a8fe18bc5317e4e02714b310
Yahor Litavar <sergioegor21@gmail.com>
2023-03-23 14:07:35 +0300

    feat: add hooks to app

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,56 +1,50 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { EPages } from './data/pages';
-import { INullProps } from './data/types';
 import About from './pages/About';
 import Cards from './pages/Cards';
 import Error from './pages/Error';
 import Form from './pages/Form';
 
-class App extends Component<INullProps, { page: string }> {
-  constructor(props: INullProps) {
-    super(props);
-    this.state = { page: '' };
-  }
+const App = () => {
+  const [page, setPage] = useState<string>('');
 
-  currentPage() {
+  const currentPage = () => {
     const path = location.pathname.split('/');
     const page = path[1];
-    if (page) this.setState({ page });
-  }
+    if (page) setPage(page);
+  };
 
-  render() {
-    const router = createBrowserRouter([
-      {
-        path: '/',
-        element: <Navbar page={this.state.page} />,
-        children: [
-          {
-            index: true,
-            element: <Navigate to={`/${EPages.ABOUT}`} replace />,
-          },
-          {
-            path: EPages.ABOUT,
-            element: <About currentPage={this.currentPage.bind(this)} />,
-          },
-          {
-            path: EPages.CARDS,
-            element: <Cards currentPage={this.currentPage.bind(this)} />,
-          },
-          {
-            path: EPages.FORM,
-            element: <Form currentPage={this.currentPage.bind(this)} />,
-          },
-        ],
-      },
-      {
-        path: '/*',
-        element: <Error />,
-      },
-    ]);
-    return <RouterProvider router={router} />;
-  }
-}
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Navbar page={page} />,
+      children: [
+        {
+          index: true,
+          element: <Navigate to={`/${EPages.ABOUT}`} replace />,
+        },
+        {
+          path: EPages.ABOUT,
+          element: <About currentPage={currentPage} />,
+        },
+        {
+          path: EPages.CARDS,
+          element: <Cards currentPage={currentPage} />,
+        },
+        {
+          path: EPages.FORM,
+          element: <Form currentPage={currentPage} />,
+        },
+      ],
+    },
+    {
+      path: '/*',
+      element: <Error />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
 
 export default App;
